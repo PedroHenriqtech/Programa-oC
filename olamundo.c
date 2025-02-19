2c4873a2924d13e64bfce745c4bb0295c88db366
PedroHenriqtech <pedrohenriquejeronimo.fs@gmail.com>
2025-02-19 15:45:48 -0300

    Update olamundo.c

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main (){
+   int idade ;
+   int quantidade = 1;
+   float altura = 1.70; 
+   double peso = 64.5;
+   char letra = 'A' ;
+   char letra [20] = "Pedro";
+
+   idade = 25
+
+
+}
